51ff486c9f593b5a44c3b9b488af82df8b8d1ae5
Tyler Pedraja <tpedraja@tylerpedraja.com>
2022-01-10 16:03:57 -0500

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -23,18 +23,30 @@ app.get(`/api/products/:id`, (req, res) => {
     })
     res.json(product);
 })
-const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join('./frontend/build')))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '/frontend', 'build', 'index.html')))
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
 } else {
-    app.get('/', (req, res) => {
-        res.send('API is running...');
-    })
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 }
 
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
