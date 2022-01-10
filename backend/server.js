3a7df3f1887654535379a55cd93b20f2d41b4e96
Tyler Pedraja <tpedraja@tylerpedraja.com>
2022-01-10 15:56:53 -0500

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,10 @@ app.get(`/api/products/:id`, (req, res) => {
 })
 const __dirname = path.resolve();
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join('./frontend/build')))
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '/frontend', 'build', 'index.html')))
@@ -33,8 +37,4 @@ if (process.env.NODE_ENV === 'production') {
         res.send('API is running...');
     })
 }
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
