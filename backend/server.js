11ed2ea36b6fc0b3bedbff58226b76902d5aab98
Tyler Pedraja <tpedraja@tylerpedraja.com>
2022-01-10 15:23:12 -0500

    heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import products from './data/products.js';
 import color from 'colors';
+import path from 'path';
 
 dotenv.config();
 
@@ -10,9 +11,8 @@ connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-})
+// Routes
+
 app.get('/api/products', (req, res) => {
     res.json(products);
 })
@@ -23,6 +23,16 @@ app.get(`/api/products/:id`, (req, res) => {
     })
     res.json(product);
 })
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join('./frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '/frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
